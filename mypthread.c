59651ac53b2aeedb6b69f61de996a1fc955bd755
Weiran Fang <weiran.fang@hotmail.com>
2015-02-11 23:45:36 -0500

    update mypthread.c

--- mypthread.c
+++ mypthread.c
@@ -5,22 +5,69 @@
 #include <stdlib.h>       // atoi(..)
 #include "mypthread-system-override.h"
 ucontext_t coroutine_main;
-ucontext_t coroutine_sub[32];
-int thread_id = 0;
-int first = 1;
-int numOfThread = 0;
-int currentId = 0;
+
+//Nodes in Doubly linked lists.
+struct Node  {
+	int id;
+	ucontext_t context;
+	struct Node* next;
+	struct Node* prev;
+};
+
+struct Node* head = NULL; // global variable - pointer to head node
+struct Node* tail = NULL; // global variable - pointer to tail node
+struct Node* currNode = NULL; // global variable - pointer to current node
+int listsize = 0;
+int totalnum = 0;
+
+//Creates a new Node and returns pointer to it. 
+struct Node* GetNewNode(ucontext_t ctx) {
+	struct Node* newNode
+		= (struct Node*)malloc(sizeof(struct Node));
+	listsize++;
+	newNode->id = listsize - 1;
+	newNode->context = ctx;
+	newNode->prev = NULL;
+	newNode->next = NULL;
+	return newNode;
+}
+
+//Inserts a Node at head of doubly linked list
+void InsertAtHead(ucontext_t ctx) {
+	struct Node* newNode = GetNewNode(ctx);
+	if(head == NULL) {
+		head = newNode;
+		tail = newNode;
+		return;
+	}
+	head->prev = newNode;
+	newNode->next = head; 
+	head = newNode;
+}
+
+//Inserts a Node at tail of Doubly linked list
+void InsertAtTail(ucontext_t ctx) {
+	//struct Node* temp = head;
+	struct Node* newNode = GetNewNode(ctx);
+	if(head == NULL) {
+		head = newNode;
+		tail = newNode;
+		return;
+	}
+        tail->next = newNode;
+	newNode->prev = tail;
+	tail = newNode;
+}
 
 int mypthread_create(mypthread_t *thread, const mypthread_attr_t *attr,
 			void *(*start_routine) (void *), void *arg){
 	// printf("start initializing a thread\n");
 	// sleep(1);
 	getcontext(&((*thread).ctx));
-    (*thread).ctx.uc_stack.ss_sp = (*thread).st;
-    (*thread).ctx.uc_stack.ss_size = sizeof((*thread).st);
-    // (*thread).ctx.uc_link = &((thread + 1) -> ctx);
-    makecontext(&((*thread).ctx), &(*start_routine), 1, arg);
-    numOfThread++;
+	(*thread).ctx.uc_stack.ss_sp = (*thread).st;
+	(*thread).ctx.uc_stack.ss_size = sizeof((*thread).st);
+	makecontext(&((*thread).ctx), &(*start_routine), 1, arg);
+	totalnum++;
 	// printf("finish initializing\n");
 
 	return 0;
@@ -32,48 +79,37 @@ void mypthread_exit(void *retval){
 }
 
 int mypthread_yield(void){
-	// printf("start yielding next thread\n");
-	// sleep(1);
-	if((thread_id <= numOfThread - 2) || (currentId <= numOfThread - 1)){
-		if(first){
-			thread_id++;
-			if(thread_id != currentId)thread_id--;
-			if(currentId == 1){
-				// printf("first one %d\n", currentId);
-				swapcontext(&coroutine_sub[currentId], &coroutine_main);
-			}
-			else{
-				// printf("backing %d\n", currentId);
-				currentId--;
-				swapcontext(&coroutine_sub[currentId + 1], &coroutine_sub[currentId]);
-			}
-		}
-		else{
-			thread_id++;
-			// printf("not first loop %d\n", thread_id);
-			swapcontext(&coroutine_sub[thread_id], &coroutine_sub[thread_id + 1]);
-		}
+	//printf("yielding thread %d\n", currNode->id);
+	if(currNode != head) 
+	{	
+		// If this thread is not the head, then return to 
+                // the previous thread in the list.
+		currNode = currNode->prev;	
+		swapcontext(&(currNode->next->context), &(currNode->context));
+	} 
+	else if(tail->id != totalnum - 1)
+	{
+		// If this thread is the head and the check thread has not yet been 
+                // added, then return to the main thread. 
+		swapcontext(&(currNode->context), &coroutine_main);
 	}
-	else if(thread_id == numOfThread - 1){
-		// printf("num of thread: %d\n", numOfThread);
-		// printf("reach the last thread %d\n", thread_id + 1);
-		thread_id = 0;
-		first = 0;
-		currentId = numOfThread;
-		swapcontext(&coroutine_sub[thread_id + numOfThread], &coroutine_sub[thread_id + 1]);
+	else
+	{
+		// If this thread is the head and the check thread has been 
+                // added to the list, then return to the check thread.
+		swapcontext(&(currNode->context), &(tail->context));
 	}
-
-
-	// printf("finish yielding\n");
+	//printf("finish yielding\n");
 
 	return 0;
 }
 
 int mypthread_join(mypthread_t thread, void **retval){
-	// printf("start joining a thread\n");
-	// sleep(1);
-	currentId = 1 + thread_id;
-	swapcontext(&coroutine_main, &(thread.ctx));
+	// join method adds a new thread onto the tail of the linked list
+	InsertAtTail(thread.ctx);
+	currNode = tail;
+
+	swapcontext(&coroutine_main, &(thread.ctx));	
 
 	return 0;
 }
